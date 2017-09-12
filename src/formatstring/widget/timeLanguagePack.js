85d41359e2606f2a7586864ca09a416d93046f0c
MulderWH <wmulder@pegamento.nl>
2017-09-12 09:20:09 +0200

    TypeError: Cannot read property 'timestampPast' of undefined
    You get TypeError: Cannot read property 'timestampPast' of undefined if you use the en-gb language in combination with the relative date format of the widget

--- src/formatstring/widget/timeLanguagePack.js
+++ src/formatstring/widget/timeLanguagePack.js
@@ -18,6 +18,24 @@ define([], function () {
             "timestampFuture": "from now",
             "timestampPast": "ago"
         },
+		"en-gb": {
+            "second": "second",
+            "seconds": "seconds",
+            "minute": "minute",
+            "minutes": "minutes",
+            "hour": "hour",
+            "hours": "hours",
+            "day": "day",
+            "days": "days",
+            "week": "week",
+            "weeks": "weeks",
+            "month": "month",
+            "months": "months",
+            "year": "year",
+            "years": "years",
+            "timestampFuture": "from now",
+            "timestampPast": "ago"
+        },
         "nl-nl": {
             "second": "seconde",
             "seconds": "seconden",
